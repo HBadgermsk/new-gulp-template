f348629b00db0f68bd6b23b97acba0e994a613ef
HBadgermsk <c-srservers@yandex.ru>
2023-10-09 14:17:28 +0300

    Start Project Gulp start template

---
+++ src/js/modules/mobile-nav.js
@@ -0,0 +1,17 @@
+const headerEl = document.getElementById("header")
+
+window.addEventListener("scroll", function () {
+  const scrollPos = window.scrollY
+
+  if (scrollPos > 100) {
+    headerEl.classList.add("header_mini")
+  } else {
+    headerEl.classList.remove("header_mini")
+  }
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function() {
+      document.querySelector("header").classList.toggle("open")
+  })
+})
